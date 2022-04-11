0a84087ea91ef7f2d2ded52e073c210b014b5925
arup <signinjio@gmail.com>
2022-04-11 20:17:40 +0530

    vchanges

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ const express = require('express');
 var bodyParser = require('body-parser');
 
 const route = require('./routes/route.js');
-
+const mongoose=require('mongoose')
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', route);
+mongoose.connect("mongodb+srv://Exlusivearup:*****@cluster0.brdgj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
+    useNewUrlParser:true
+})
+.then(()=>console.log("Mongodb is connected")
+  .catch(err=>console.log(err))
+
+ app.use('/', route);
 
 app.listen(process.env.PORT || 4000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 4000))
