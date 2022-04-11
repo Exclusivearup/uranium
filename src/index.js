8a1b2286e590dd87341b8c9cba1622ab5cd5cc36
arup <signinjio@gmail.com>
2022-04-12 01:04:43 +0530

    changes-2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Exlusivearup:Arup+patra@1@cluster0.brdgj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,19 +1,15 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema( {
-    firstName: String,
-    lastName: String,
-    mobile: {
+    bookName: String,
+    authorName: String,
+
+    category: {
         type: String,
-        unique: true,
-        required: true
+        enum: ["Novel", "Biography", "Drama"] //"falana" will give an error
     },
-    emailId: String,
-    gender: {
-        type: String,
-        enum: ["male", "female", "LGBTQ"] //"falana" will give an error
-    },
-    age: Number,
+    year: Number,
+   
     // isIndian: Boolean,
     // parentsInfo: {
     //     motherName: String,
